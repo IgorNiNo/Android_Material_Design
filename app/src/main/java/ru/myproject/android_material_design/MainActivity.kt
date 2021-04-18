ac01969c12f7a7b76487d6b4064dbd72b8c4ba8d
IgorNiNo <vis_74@mail.ru>
2021-04-18 18:56:38 +0300

    Initial commit

---
+++ app/src/main/java/ru/myproject/android_material_design/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.myproject.android_material_design
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.myproject.android_material_design.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ru/myproject/android_material_design/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.myproject.android_material_design.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
